689b091b22a71f096d12d654a152e258033e99d5
Himank Pathak <himankpathak@gmail.com>
2018-12-13 23:17:43 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tic-Tac-Toe
+simple tictactoe game made in python3 which can be played on terminal
+
---
+++ tictactoe.py
@@ -0,0 +1,15 @@
+
+
+if __name__ == "__main__":
+	l=["_","_","_","_","_","_"," "," "," "]
+	n=0
+	print(""" Instructions to input:
+					
+		a|b|c
+		d|e|f
+		g|h|i \n""")
+
+	name=[]
+	name.append(raw_input("Enter the Name of First Player : "))
+	name.append(raw_input("Enter the Name of Second Player: "))
+
