fd697ddcd999d89afca45511a6b650b7a8642f2b
Himank Pathak <himankpathak@gmail.com>
2018-12-13 23:25:14 +0530

    added print function to display the board
    also corrected raw_input to input so it may work in python3

--- tictactoe.py
+++ tictactoe.py
@@ -1,15 +1,22 @@
 
+def printlist():
+	print ("\n")
+	print (l[0],"|",l[1],"|",l[2])
+	print (l[3],"|",l[4],"|",l[5])
+	print (l[6],"|",l[7],"|",l[8])
+	print ("\n")
 
 if __name__ == "__main__":
 	l=["_","_","_","_","_","_"," "," "," "]
 	n=0
-	print(""" Instructions to input:
+	print("""    Instructions to input:
 					
 		a|b|c
 		d|e|f
 		g|h|i \n""")
 
 	name=[]
-	name.append(raw_input("Enter the Name of First Player : "))
-	name.append(raw_input("Enter the Name of Second Player: "))
+	name.append(input("Enter the Name of First Player : "))
+	name.append(input("Enter the Name of Second Player: "))
 
+	printlist()
